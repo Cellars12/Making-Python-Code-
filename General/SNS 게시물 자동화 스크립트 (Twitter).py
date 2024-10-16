a4e2abfaefe6b33166e8c2d8b848b6bfa741ce0c
Cellars <120205903+Cellars12@users.noreply.github.com>
2024-10-16 23:00:56 +0900

    Create SNS 게시물 자동화 스크립트 (Twitter).py

---
+++ General/SNS 게시물 자동화 스크립트 (Twitter).py
@@ -0,0 +1,15 @@
+import tweepy
+
+def tweet(text):
+    consumer_key = 'your_consumer_key'
+    consumer_secret = 'your_consumer_secret'
+    access_token = 'your_access_token'
+    access_token_secret = 'your_access_token_secret'
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    
+    api.update_status(text)
+
+tweet("트윗 내용을 작성해주세요.")
